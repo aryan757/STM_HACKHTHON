3e90ac55ad928bc1ddfd382e06417037ee13a928
aryan215gupta <aryan215gupta@gmail.com>
2024-03-15 12:08:42 +0530

    first commit

---
+++ Data_path/directory_path.py
@@ -0,0 +1,11 @@
+
+
+
+class regression_data_path():
+    def __init__(self):
+        pass
+    def directory_path_regression_file(self):
+        return "C:\\Users\\aryan\\Data_Hackthon\\PROJECT1\\REGR_STATUS_RTL"
+
+    def directory_path_coverage_file(self):
+        return "C:\\Users\\aryan\\Data_Hackthon\\PROJECT1\\COV_STATUS_TOGGLE"
